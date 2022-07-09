0e745acdfb656924436866800a36920f408bc31c
Aviral Sharma <aviralsharma2021@iitkalumni.org>
2022-07-09 12:16:06 +0530

    updated cleaning.sql

--- cleaning.sql
+++ cleaning.sql
@@ -31,36 +31,36 @@ SET Make = UPPER(Make);
 
 --	Changing Location to Upper Case
 UPDATE AviationData
-SET Location = UPPER(Location)
+SET Location = UPPER(Location);
 
 
 
 --				Splitting Location to Area and State for US, Area and country for non-US locations
 ALTER TABLE AviationData
-ADD state_country nvarchar(255)
+ADD state_country nvarchar(255);
 
 UPDATE AviationData
 SET Location = SUBSTRING(Location, 1, CHARINDEX(',', Location)-1)
-WHERE Location LIKE '%,%'		-- Takes care of locations where state or country is missing
+WHERE Location LIKE '%,%';		-- Takes care of locations where state or country is missing
 
 UPDATE AviationData
 SET state_country = TRIM(SUBSTRING(Location, CHARINDEX(',', Location)+1, len(Location)))
-WHERE Location LIKE '%,%'
+WHERE Location LIKE '%,%';
 
 --	Checking state_country to be correct after split
 SELECT DISTINCT state_country
 FROM AviationData
-WHERE Country = 'United States' AND LEN(state_country) !=2 AND state_country NOT IN ('LA,', '')
+WHERE Country = 'United States' AND LEN(state_country) !=2 AND state_country NOT IN ('LA,', '');
 
 --	Correcting state where state_country is 'LA,'
 UPDATE AviationData
 SET state_country = 'LA'
-WHERE state_country = 'LA,'
+WHERE state_country = 'LA,';
 
 --	Corrective other US states with additional location info
 UPDATE AviationData
 SET state_country = TRIM(SUBSTRING(state_country, CHARINDEX(',', state_country)+1, len(state_country)))
-WHERE state_country LIKE '%,%' AND Country = 'United States' AND LEN(state_country) !=2 AND state_country != 'LA,'
+WHERE state_country LIKE '%,%' AND Country = 'United States' AND LEN(state_country) !=2 AND state_country != 'LA,';
 
 
 
@@ -99,16 +99,16 @@ SELECT  Event#Id,
 		Total#Uninjured,
 		Weather#Condition,
 		Broad#phase#of#flight
-INTO aviation_accidents FROM AviationData
+INTO aviation_accidents FROM AviationData;
 
 SELECT *
-FROM aviation_accidents
+FROM aviation_accidents;
 
 
 
 --				Dealing with missing values
 SELECT *
-FROM aviation_accidents
+FROM aviation_accidents;
 
 
 --	Total Null Values per column
@@ -142,12 +142,12 @@ SELECT 'Total#Uninjured', SUM(CASE WHEN Total#Uninjured IS NULL THEN 1 ELSE 0 EN
 UNION ALL
 SELECT 'Weather#Condition', SUM(CASE WHEN Weather#Condition IS NULL THEN 1 ELSE 0 END) FROM aviation_accidents
 UNION ALL
-SELECT 'Broad#phase#of#flight', SUM(CASE WHEN Broad#phase#of#flight IS NULL THEN 1 ELSE 0 END) FROM aviation_accidents
+SELECT 'Broad#phase#of#flight', SUM(CASE WHEN Broad#phase#of#flight IS NULL THEN 1 ELSE 0 END) FROM aviation_accidents;
 
 
 --	Records where Injury Information is not available is not valuable
 DELETE FROM aviation_accidents
-WHERE Total#Fatal#Injuries is null or Total#Minor#Injuries is null or Total#Serious#Injuries is null or  Total#Uninjured is null  
+WHERE Total#Fatal#Injuries is null or Total#Minor#Injuries is null or Total#Serious#Injuries is null or  Total#Uninjured is null;  
 
 
 /*
@@ -158,114 +158,114 @@ Also to be set unknown - injuryseverity, aircraft damage, engine_type, purpose o
 
 UPDATE aviation_accidents
 SET Location = 'UNKNOWN'
-WHERE Location IS NULL
+WHERE Location IS NULL;
 
 UPDATE aviation_accidents
 SET Country = 'UNKNOWN'
-WHERE Country IS NULL
+WHERE Country IS NULL;
 
 UPDATE aviation_accidents
 SET state_country = 'UNKNOWN'
-WHERE state_country IS NULL
+WHERE state_country IS NULL;
 
 UPDATE aviation_accidents
 SET Make = 'UNKNOWN'
-WHERE Make IS NULL
+WHERE Make IS NULL;
 
 UPDATE aviation_accidents
 SET Model = 'UNKNOWN'
-WHERE Model IS NULL
+WHERE Model IS NULL;
 
 
 --	Weather#Condition has a value of UNK which means Unknown, set to UNKNOWN for uniformity
 SELECT DISTINCT Weather#Condition
-FROM aviation_accidents
+FROM aviation_accidents;
 
 UPDATE aviation_accidents
 SET Weather#Condition = 'UNKNOWN'
-WHERE Weather#Condition IS NULL OR Weather#Condition='UNK'
+WHERE Weather#Condition IS NULL OR Weather#Condition='UNK';
 
 
 --	Broad#phase#of#flight has a value of Unknown, set to UNKNOWN for uniformity
 SELECT DISTINCT Broad#phase#of#flight
-FROM aviation_accidents
+FROM aviation_accidents;
 
 UPDATE aviation_accidents
 SET Broad#phase#of#flight = 'UNKNOWN'
-WHERE Broad#phase#of#flight IS NULL OR Broad#phase#of#flight='Unknown'
+WHERE Broad#phase#of#flight IS NULL OR Broad#phase#of#flight='Unknown';
 
 
 --	For some data, where Injury#Severity is Fatal, it has a value total deaths in bracket
 SELECT Injury#Severity, Total#Fatal#Injuries
 FROM aviation_accidents
-WHERE Injury#Severity LIKE 'Fatal%'
+WHERE Injury#Severity LIKE 'Fatal%';
 
 UPDATE aviation_accidents
 SET Injury#Severity = SUBSTRING(Injury#Severity, 1, CHARINDEX('(', Injury#Severity)-1)
-WHERE Injury#Severity LIKE 'Fatal(%'
+WHERE Injury#Severity LIKE 'Fatal(%';
 
 SELECT DISTINCT Injury#Severity
-FROM aviation_accidents
+FROM aviation_accidents;
 
 UPDATE aviation_accidents
 SET Injury#Severity = 'UNKNOWN'
-WHERE Injury#Severity IS NULL OR Injury#Severity='Unavailable'
+WHERE Injury#Severity IS NULL OR Injury#Severity='Unavailable';
 
 
 --	Aircraft#damage has a value of Unknown, set to UNKNOWN for uniformity
 SELECT DISTINCT Aircraft#damage
-FROM aviation_accidents
+FROM aviation_accidents;
 
 UPDATE aviation_accidents
 SET Aircraft#damage = 'UNKNOWN'
-WHERE Aircraft#damage IS NULL OR Aircraft#damage='Unknown'
+WHERE Aircraft#damage IS NULL OR Aircraft#damage='Unknown';
 
 
 --	Engine#Type has a value of Unknown, set to UNKNOWN for uniformity
 SELECT DISTINCT Engine#Type
-FROM aviation_accidents
+FROM aviation_accidents;
 
 UPDATE aviation_accidents
 SET Engine#Type = 'UNKNOWN'
-WHERE Engine#Type IS NULL OR Engine#Type='Unknown'
+WHERE Engine#Type IS NULL OR Engine#Type='Unknown';
 
 
 --	Purpose#of#flight has a value of Unknown, set to UNKNOWN for uniformity
 SELECT DISTINCT Purpose#of#flight
-FROM aviation_accidents
+FROM aviation_accidents;
 
 UPDATE aviation_accidents
 SET Purpose#of#flight = 'UNKNOWN'
-WHERE Purpose#of#flight IS NULL OR Purpose#of#flight='Unknown'
+WHERE Purpose#of#flight IS NULL OR Purpose#of#flight='Unknown';
 
 
 --	Setting Number#of#Engines = 2, as it is most common
 UPDATE aviation_accidents
 SET Number#of#Engines = 2
-WHERE Number#of#Engines IS NULL
+WHERE Number#of#Engines IS NULL;
 
 
 --				Add survival rate
 
 ALTER TABLE aviation_accidents
-ADD survival_rate float
+ADD survival_rate float;
 
 UPDATE aviation_accidents
 SET survival_rate = (Total#Minor#Injuries + Total#Serious#Injuries + Total#Uninjured) 
 					/ (Total#Minor#Injuries + Total#Serious#Injuries + Total#Uninjured + Total#Fatal#Injuries) * 100
-WHERE (Total#Minor#Injuries + Total#Serious#Injuries + Total#Uninjured + Total#Fatal#Injuries) != 0
+WHERE (Total#Minor#Injuries + Total#Serious#Injuries + Total#Uninjured + Total#Fatal#Injuries) != 0;
 
 DELETE FROM aviation_accidents
-WHERE survival_rate IS NULL
+WHERE survival_rate IS NULL;
 
 SELECT *
 FROM aviation_accidents
-ORDER BY survival_rate
+ORDER BY survival_rate;
 
 
 --				Add Total Injuries
 ALTER TABLE aviation_accidents
-ADD total_injuries float
+ADD total_injuries float;
 
 UPDATE aviation_accidents
-SET total_injuries = Total#Minor#Injuries + Total#Serious#Injuries + Total#Fatal#Injuries
+SET total_injuries = Total#Minor#Injuries + Total#Serious#Injuries + Total#Fatal#Injuries;
